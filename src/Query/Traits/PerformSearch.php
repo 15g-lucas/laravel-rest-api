e579ada4ea1b32065d02a9f4c0397a7c2436f451
15glucas <gosgnach24lucas@gmail.com>
2026-02-05 19:52:12 +0100

    Update test

--- src/Query/Traits/PerformSearch.php
+++ src/Query/Traits/PerformSearch.php
@@ -245,7 +245,7 @@ trait PerformSearch
 
 
 
-        return $this->queryBuilder->disableDefaultLimit()->withAggregate([$relation => function (Builder $query) use ($aggregate) {
+        return $this->queryBuilder->withAggregate([$relation => function (Builder $query) use ($aggregate) {
             $resource = $this->resource->relation($aggregate['relation'])?->resource();
 
             $queryBuilder = $this->newQueryBuilder(['resource' => $resource, 'query' => $query]);
